ff874fb2d3ccc455c9f1a82746142a2ee7db2c65
Mallik k <102137113+arjun-jar@users.noreply.github.com>
2022-07-08 12:10:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Form from './components/Form';
-import TodoList  from './components/TodoList';
+import Header from './component/Header';
+import Form from './component/Form';
+import TodoList from './component/TodoList';
  
 
 const App=()=>{
-  const[input,setInput]=useState('')
+  const[input,setInput]=useState()
   const[todo,setTodo]=useState([])
   return (
     <div className="App">
    <Header/>
      <Form input={input} setInput={setInput} todo={todo} setTodo={setTodo}/>
-        <TodoList todo={todo} setTodo={setTodo} />
+        <TodoList todo={todo} setTodo={setTodo} input={input} setInput={setInput}  />
     </div>
   );
 }
